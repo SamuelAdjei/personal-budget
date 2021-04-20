9f60f9db8caf3edc07a4529831323f4b21f9988c
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2021-04-20 21:25:07 +0100

    updated base server

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require('express');
-const dotenv = require('dotenv')
-const app = express()
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const app = express();
 
-app.get('/', (req, res) => {
-    console.log(`GET data`)
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// get an instance of the express Router
+const router = express.Router();
+
+// middleware to use for all requests
+router.use((req, res, next) => {
+    next();
 });
 
-app.post('/', (req, res) => {
-    res.send()
+
+
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'hooray! welcome to our api!' })
 });
 
+router.post('/', (req, res) => {
+    res.status(201).json()
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with '/api'
+app.use('/api', router);
 
 const port = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 })
 
